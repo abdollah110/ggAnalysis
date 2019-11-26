ad638be5b81188c6faa27c90d1d13d51a49c3ffb
abdollah <smohamadi@gmail.com>
2019-11-26 16:38:50 -0600

    add recoil correction

--- ggNtuplizer/interface/ggNtuplizer.h
+++ ggNtuplizer/interface/ggNtuplizer.h
@@ -179,7 +179,8 @@ class ggNtuplizer : public edm::EDAnalyzer {
   //EnergyScaleCorrection_class *egmScaler_;
     
 //   static const RecoilCorrector recoilPFMetCorrector;
-   static const RecoilCorrector recoilPFMetCorrector("HTT-utilities/RecoilCorrections/data/Type1_PFMET_2017.root"), // Type I PF MET 2017,
+//   static const RecoilCorrector recoilPFMetCorrector("HTT-utilities/RecoilCorrections/data/Type1_PFMET_2017.root"), // Type I PF MET 2017,
+   static const RecoilCorrector recoilPFMetCorrector("HTT-utilities/RecoilCorrections/data/Type1_PFMET_2017.root"); // Type I PF MET 2017,
 
   JME::JetResolution            jetResolution_;
   JME::JetResolutionScaleFactor jetResolutionSF_;
